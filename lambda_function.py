f8f74ffeabeff807b49c7750b99f3c34c89ff574
jinsu9758 <60132287+jinsu9758@users.noreply.github.com>
2023-12-01 23:01:21 +0900

    Upload lambda source code
    - Trigger SQS service and send data to EC2 server

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import json
+import os
+import urllib.request
+
+def lambda_handler(event, context):
+    print("event : ", event)
+    url = os.environ['web_url']
+    print("url : ", url)
+    
+    charge_cash = event["Records"][0]["body"]
+    charge_cash = json.loads(charge_cash)
+    
+    if charge_cash["charge_amount"]:
+        try:
+            path = '/sqs_process'
+            data_json = json.dumps(charge_cash).encode('utf-8')
+            req = urllib.request.Request(url+path, data=data_json, headers={'content-type': 'application/json'})
+            res = urllib.request.urlopen(req)
+            res_data = response.read().decode('utf-8')
+            return "Message sent to EC2 server successfully!"
+            
+        except Exception as e:
+            return "Error sending request to EC2 server"
+    else:
+        return "another request"
